4491e7c40c72322e6365f8bdd9e0c0a9020d78ea
mrvnzcn <imervanozcan@gmail.com>
2024-05-18 15:14:35 +0300

    extra user info

--- Models/IdentitySeedData.cs
+++ Models/IdentitySeedData.cs
@@ -5,7 +5,7 @@ namespace IdentityApp.Models
 {
     public static class IdentitySeedData
     {
-        private const string adminUser = "Admin";
+        private const string adminUser = "admin";
         private const string adminPassword = "Admin_123";
         public static async void IdentityTestUser(IApplicationBuilder app)
         {
@@ -16,12 +16,13 @@ namespace IdentityApp.Models
                 context.Database.Migrate();
             }
 
-            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<AppUser>>();
 
             var user = await userManager.FindByNameAsync(adminUser);
             if(user == null)
             {
-                user = new IdentityUser {
+                user = new AppUser {
+                    FullName = "Mervan Özcan",
                     UserName = adminUser,
                     Email = "admin@mervanozcan.com",
                     PhoneNumber = "905349318717",
