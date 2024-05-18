e9b4358d87ec35328ceb0a57ca38ad3b80529332
mrvnzcn <imervanozcan@gmail.com>
2024-05-18 22:47:28 +0300

    users edit role list

--- ViewModels/EditViewModel.cs
+++ ViewModels/EditViewModel.cs
@@ -18,6 +18,8 @@ namespace IdentityApp.ViewModels
         [Compare("Password", ErrorMessage = "Parola eşleşmiyor.")]
         public string? ConfirmPassword { get; set; }
 
+        public IList<string>? SelectedRoles { get; set; }
+
 
     }
 }
